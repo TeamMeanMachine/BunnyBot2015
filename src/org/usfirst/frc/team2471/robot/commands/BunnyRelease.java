f6a9ed301c028498af69e98e355eeb8d1d113f69
Shawn Hice <shawnhice@gmail.com>
2015-12-12 20:55:37 -0800

    Bunny Grabber
    Madeline's first commands.  So excited.

---
+++ src/org/usfirst/frc/team2471/robot/commands/BunnyRelease.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team2471.robot.commands;
+
+import org.usfirst.frc.team2471.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class BunnyRelease extends Command {
+
+    public BunnyRelease() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	RobotMap.bunnyGrabber.set(false);    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
