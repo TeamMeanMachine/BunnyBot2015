a02eea0fc4137bb4e20ccfe60cf0d679205490b3
Shawn Hice <shawnhice@gmail.com>
2015-12-17 17:46:53 -0800

    Auto contains new RotateCommand

--- src/org/usfirst/frc/team2471/robot/commands/BunnyBotAuto.java
+++ src/org/usfirst/frc/team2471/robot/commands/BunnyBotAuto.java
@@ -1,37 +1,41 @@
 package org.usfirst.frc.team2471.robot.commands;
 
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
  *
  */
 public class BunnyBotAuto extends CommandGroup {
-    
-    public  BunnyBotAuto() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
 
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
+	public BunnyBotAuto() {
+		// Add Commands here:
+		// e.g. addSequential(new Command1());
+		// addSequential(new Command2());
+		// these will run in order.
 
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
-    	
-    	addSequential( new OrbitalDown());
-    	addSequential( new BunnyRelease());
-    	addParallel( new SuckUpOnly(), 2.0);  // 2 seconds
-    	addSequential( new DriveDistanceCommand(1.0, 0.0, 0.6), 0.25);  // forward 1 foot
-    	addSequential( new BunnyGrab());
+		// To run multiple commands at the same time,
+		// use addParallel()
+		// e.g. addParallel(new Command1());
+		// addSequential(new Command2());
+		// Command1 and Command2 will run in parallel.
 
-    	addSequential( new DriveDistanceCommand(10.0, 0.0, 0.6), 3.0);  // test with 10 feet
-    	//addSequential( new DriveDistanceCommand(30.0, 0.0, 0.6), 9.0);  // forward 30 feet
-    }
+		// A command group will require all of the subsystems that each member
+		// would require.
+		// e.g. if Command1 requires chassis, and Command2 requires arm,
+		// a CommandGroup containing them would require both the chassis and the
+		// arm.
+		addParallel(new BunnyRelease());
+		addSequential(new OrbitalDown());
+		addParallel(new DriveDistanceCommand(2.0, 0.0, 0.2), 0.25); // forward 2
+																	// foot
+		addSequential(new SuckUpOnly(), 0.6);
+		addParallel(new OrbitalUp());
+		addSequential(new BunnyGrab());
+
+		addSequential(new DriveDistanceCommand(30.0, 0.0, 0.2), 10.0); // forward
+																		// 30
+																		// feet
+		addSequential(new RotateCommand(180.0, 4.0));
+	}
 }
---
+++ src/org/usfirst/frc/team2471/robot/commands/RotateCommand.java
@@ -0,0 +1,65 @@
+package org.usfirst.frc.team2471.robot.commands;
+
+import org.usfirst.frc.team2471.robot.Robot;
+import org.usfirst.frc.team2471.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class RotateCommand extends Command {
+
+	double distance;
+    boolean started = false;
+    double startLeft, startRight;
+    double speed;
+    
+    public RotateCommand(double angle, double _speed) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(Robot.drive);
+        distance = angle/360.0*27.0/12.0*Math.PI;
+        speed = _speed;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        if(!started) {
+        	startLeft = RobotMap.leftE.getDistance();
+        	startRight = RobotMap.rightE.getDistance();
+            started = true;
+            Robot.drive.leftController.setSetpoint(-Math.signum(distance) * speed);
+    		Robot.drive.rightController.setSetpoint(Math.signum(distance) * speed);
+        }
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	if (Math.abs(startLeft - RobotMap.leftE.getDistance())> distance){
+    		return true;
+    	}
+    	if (Math.abs(startRight - RobotMap.rightE.getDistance())> distance){
+    		return true;
+    	}
+
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+        started = false;
+        Robot.drive.leftController.setSetpoint(0);
+		Robot.drive.rightController.setSetpoint(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
